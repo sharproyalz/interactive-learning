e679bc3e4ce5cb84126d770743d14c36b4327eef
sharproyalz <vopactol@gmail.com>
2024-08-24 17:55:04 +0800

    chore: change the page from login to study timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { redirect } from "next/navigation";
 import { paths } from "~/paths";
 
 export default function HomePage() {
-  redirect(`${paths.AUTH}${paths.LOGIN}`);
+  // redirect(`${paths.AUTH}${paths.LOGIN}`);
+  redirect(`${paths.STUDY_TIMER}`);
 
   return <section className=""></section>;
 }
--- src/paths.js
+++ src/paths.js
@@ -2,4 +2,5 @@ export const paths = {
   AUTH: "/auth",
   LOGIN: "/login",
   SIGNUP: "/signup",
+  STUDY_TIMER: "/study-timer",
 };
